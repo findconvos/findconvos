feb4bdf00a5bed9466a6eaa297c7dd8964d9df9e
Colin Brauns <colin@ardour.io>
2016-08-27 17:58:00 -0400

    no smooth scroll <3 @adamschwartz

--- js/custom.js
+++ js/custom.js
@@ -112,20 +112,6 @@ $(document).ready(function() {
 });
 
 
-/* =================================
-===  SMOOTH SCROLL             ====
-=================================== */
-var scrollAnimationTime = 1200,
-    scrollAnimation = 'easeInOutExpo';
-$('a.scrollto').bind('click.smoothscroll', function (event) {
-    event.preventDefault();
-    var target = this.hash;
-    $('html, body').stop().animate({
-        'scrollTop': $(target).offset().top
-    }, scrollAnimationTime, scrollAnimation, function () {
-        window.location.hash = target;
-    });
-});
 
 
 /* =================================
